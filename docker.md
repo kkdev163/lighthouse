6323925996301e5f72d3688c69d810d6b69dd419
朱坤坤 <hzzhukunkun@corp.netease.com>
2019-08-09 20:38:34 +0800

    feat: 修改entrypoint配置

--- docker.md
+++ docker.md
@@ -11,11 +11,12 @@ docker build -t webapm/google-chrome-headless-node -f Dockerfile.node --build-ar
 
 之后改动Lighthouse源码，只需要构建最顶层的镜像即可。
 ```
-docker build -t webapm/lighthouse:5.2.0-190805 -f Dockerfile --build-arg VERSION=5.2.0-190805 --no-cache=1 .
+docker build -t webapm/lighthouse:5.2.0-190809 -f Dockerfile --build-arg VERSION=5.2.0-190809 --no-cache=1 .
 ```
 
 ### 镜像使用
 参数是透传给lighthouse的，所以可以用如下命令:
 ```
-docker run webapm/lighthouse:5.2.0-190805 --help
+docker run --privileged -v /tmp/lighthouse:/home/chrome/reports webapm/lighthouse:5.2.0-190809  --chrome-flags=\'--headless --disable-gpu\' chrome://version
 ```
+报告可在host机上的/tmp/lighthouse目录查看到
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,13 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#ip}" = "$1" ]; then
-    command="sudo -H -u chrome sh -c \"lighthouse --enable-error-reporting $@\""
+    # --enable-error-reporting=1 可以在后面直接跟url, 否则url会被认为是enable-error-reporting的值
+    command="sudo -H -u chrome sh -c \"lighthouse --enable-error-reporting=1 $@\""
     exec sh -c "$command"
 else
     # pass extra command at position 0, such as network limit (tc qdisc add dev eth0 root tbf rate 4mbit peakrate 8mbit burst 64kb latency 50ms minburst 1540)
     first_arg="$1"
     shift
-    command="$first_arg && sudo -H -u chrome sh -c \"lighthouse --enable-error-reporting $@\""
+    command="$first_arg && sudo -H -u chrome sh -c \"lighthouse --enable-error-reporting=1 $@\""
     exec sh -c "$command"
 fi
